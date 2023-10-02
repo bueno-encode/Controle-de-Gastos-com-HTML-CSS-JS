28ca45c75c0baa0b15839327a6052751dd2a26e0
bueno <santos.wellingtonbueno@gmail.com>
2023-10-01 23:23:57 -0300

    Atualização

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 function validateFields() {
-    // pegar o valor do campo email
-    // verificar se este valor não é vazio e se o email e valido 
-    // se verdadeiro, habilito botao de recuperar senha
-    // se falso, então desabilitar o botão de recuperar senha
     const emailValid = isEmailValid();
-    document.getElementById('recover-password-button').value = !emailValid; 
-    
+    document.getElementById('recover-password-button').disabled = !emailValid;     
+
     const passwordValid = isPasswordValid();
-    document.getElementById('login-buton').disable = !emailValid || !passwordValid;
+    document.getElementById('login-buton').disabled = !emailValid || !passwordValid;
 }
 
 function isEmailValid() {
     const email = document.getElementById("email").value;
-    if (!email){
+    if (!email) {
         return false;
     } 
         return validateEmail(email);
@@ -20,7 +16,7 @@ function isEmailValid() {
 
 function isPasswordValid() {
     const password = document.getElementById('password').value;
-    if(!password){
+    if (!password) {
         return false;
     }
     return true;
